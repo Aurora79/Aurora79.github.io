fa3ebe15f7528b5c9e29eb7aa48553ff1c1a774a
Aurora79 <51771632+Aurora79@users.noreply.github.com>
2019-06-14 15:44:11 +0800

    Add files via upload

---
+++ myworks/data.js
@@ -0,0 +1,1 @@
+var myData={designer:{name:'皮雪',phone:'16607477880',tag:["#刺客型设计师#可可爱爱#爱生活#爱设计#爱美食"],bg:'#ac30ef'},gallery:[{src:"myworks/插画7.jpg",w:1600,h:1200},{src:"myworks/插画.png",w:1600,h:1200},{src:"myworks/插画5.jpg",w:1600,h:1200},{src:"myworks/插画6.jpg",w:1600,h:1200},{src:"myworks/插画4.jpg",w:3306,h:2479},{src:"myworks/插画2.png",w:1600,h:1200},{src:"myworks/插画3.png",w:1600,h:1200},]}
